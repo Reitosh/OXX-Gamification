491fbab0fb189e1e026a3ebee36851b8bff27986
vlad095 <41382192+vlad095@users.noreply.github.com>
2020-02-17 13:39:10 +0100

    Vlad update

---
+++ OXXGame/OXXGame/wwwroot/js/nav_scroll.js
@@ -0,0 +1,19 @@
+﻿/**********************************************/
+/********* OXX Game JavaScript File ***********/
+/******* Filename: client-validation.js *******/
+/**********************************************/
+
+$(window).scroll(function () {
+    $('nav').toggleClass('navbar-scroll', $(this).scrollTop() > 88);
+
+    if ($(this).scrollTop() > 88) {
+        $('.navbar .navbar-brand img').attr('src', '../images/brand/logo_svart.png');
+        $('.navbar .navbar-brand img').css('width', '36px');
+        $('.navbar-nav li a').css('color', '#282828');
+    } else {
+        $('.navbar .navbar-brand img').attr('src', '../images/brand/logo_hvit.png');
+        $('.navbar .navbar-brand img').css('width', '42px');
+        $('.navbar-nav li a').css('color', '#ffffff');
+    }
+
+});
