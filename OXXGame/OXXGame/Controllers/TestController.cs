220964bc6cec535cb7c97a7f52808221a70561fb
Wellerop <markuswell@msn.com>
2020-05-07 19:51:20 +0200

    Små endinger

--- OXXGame/OXXGame/Controllers/TestController.cs
+++ OXXGame/OXXGame/Controllers/TestController.cs
@@ -86,6 +86,7 @@ namespace OXXGame.Controllers
                 if (Submit(testModel))
                 {
                     ModelState.Clear();
+                    ViewData["Output"] = null;
                     TestModel newModel = getModel();
 
                     if (newModel == null)
@@ -367,6 +368,11 @@ namespace OXXGame.Controllers
         // Besvarelse underkjennes ved feilmelding fra (en av følgende): C#-kompilator, C#-test, TS-kompilator
         public void RunCode(TestModel testModel)
         {
+            if (testModel.code == null)
+            {
+                testModel.singleTestResult.passed = SingleTestResult.NOT_PASSED;
+                return;
+            }
             SSHConnect ssh = new SSHConnect("Markus", "Plainsmuchj0urney", "51.140.218.174");
             string output = ssh.RunCode(testModel);
 
@@ -393,13 +399,23 @@ namespace OXXGame.Controllers
                 testModel.endTime = DateTime.Now;
                 testModel.singleTestResult.timeSpent = (testModel.endTime - testModel.startTime).ToString(@"hh\:mm\:ss");
 
-                FileHandler fileHandler = new FileHandler(/*@"C:\Users\siver\Desktop\oxxgameFileTest",true*/);
+                FileHandler fileHandler = new FileHandler(/*@"C:\Users\siver\Desktop\oxxgameFileTest", true*/);
                 string relativePath = string.Format("/{0}", HttpContext.Session.GetInt32(userId));
-                string fileName = string.Format(
+                string fileName;
+
+                //if (testModel.task.category == "HTML" || testModel.task.category == "CSS" || 
+                //    testModel.task.category == "JavaScript" || testModel.task.category == "Vue.js")
+                //{
+                //    fileName = string.Format("{0}_Ex{1}.html", testModel.task.category, testModel.task.testId);
+                //} 
+                //else
+                //{
+                    fileName = string.Format(
                     "{0}_Ex{1}",
                     testModel.task.category,
                     testModel.task.testId
                     );
+                //}
 
                 if (fileName.StartsWith(".")) { fileName = fileName.Replace(".", "dot"); }
 
