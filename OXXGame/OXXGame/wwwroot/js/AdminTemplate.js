45afea021969ddba8f64028375b2cd112faeece2
Wellerop <markuswell@msn.com>
2020-05-12 17:48:03 +0200

    Templates kan nå skrives som Admin

---
+++ OXXGame/OXXGame/wwwroot/js/AdminTemplate.js
@@ -0,0 +1,11 @@
+﻿var Templates = CodeMirror.fromTextArea(
+    document.getElementById('templatess'), {
+    theme: "base16-dark",
+    lineNumbers: true,
+    identUnit: 4,
+    indentWithTabs: true,
+    indentWithTags: true,
+    autoCloseBrackets: true,
+    scrollbarStyle: "overlay",
+    placeholder: "Template"
+});
